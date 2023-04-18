4516dc04a5d4f28a54775257f4d851003976b0cb
Bojan Vreco <milosgak2020@gmail.com>
2023-04-17 20:45:03 -0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'themeGreen': '#41BA03',
+      'themeBlue': '#004195',
+      'themeLightBlue': '#15A1E9',
+      'white': '#FFFFFF',
+    },
+  },
+  plugins: [],
+}
+
